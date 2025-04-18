1253e94a1b0b8d7f53cd0d6ae1e7d6c7f5194d6d
shshamim63 <shshamim63@gmail.com>
2025-04-18 22:43:59 +0600

    Fixed protected route refreshtoken issue

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import styled from "styled-components";
@@ -7,6 +5,8 @@ import styled from "styled-components";
 import Spinner from "./Spinner";
 
 import { useAuth } from "../features/authentication/useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const FullPage = styled.div`
   height: 100vh;
@@ -17,27 +17,14 @@ const FullPage = styled.div`
 `;
 
 const ProtectedRoute = ({ children }) => {
+  const { accessToken } = useAuth();
   const navigate = useNavigate();
-  const { currentAccessToken, refreshAccessToken } = useAuth();
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasTriedRefresh, setHasTriedRefresh] = useState(false);
 
   useEffect(() => {
-    const checkToken = async () => {
-      if (!currentAccessToken && !hasTriedRefresh) {
-        const newToken = await refreshAccessToken();
-        setHasTriedRefresh(true);
-        if (!newToken) {
-          navigate("/login", { replace: true });
-        }
-      }
-      setIsLoading(false);
-    };
+    if (!accessToken) navigate("/login", { replace: true });
+  }, [accessToken, navigate]);
 
-    checkToken();
-  }, [currentAccessToken, hasTriedRefresh, navigate, refreshAccessToken]);
-
-  if (isLoading || (!currentAccessToken && !hasTriedRefresh)) {
+  if (!accessToken) {
     return (
       <FullPage>
         <Spinner />

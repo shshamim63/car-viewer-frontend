5507fadb3d8c346d2befe374f211df8682621e50
shshamim63 <shshamim63@gmail.com>
2025-04-19 23:08:13 +0600

    Updated axios instance to set the accessToken

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 import { API_URL } from "../utils/constants";
 
+let accessToken = null;
+
+export const setAxiosAccessToken = (token) => {
+  accessToken = token;
+};
+
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
 });

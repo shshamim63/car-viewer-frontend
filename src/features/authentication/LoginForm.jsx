2ebb4a59cf633acc34ff4006bdc67c6d3b687e2d
shshamim63 <shshamim63@gmail.com>
2025-04-22 20:57:25 +0600

    Updated the Login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import Form from "../../ui/Form";
-import FormRowVertical from "../../ui/FormRowVertical";
-import Input from "../../ui/Input";
-import Button from "../../ui/Button";
 import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
+import { Stack, TextField, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -30,33 +29,38 @@ const LoginForm = () => {
 
   return (
     <Form onSubmit={handleOnSubmit}>
-      <FormRowVertical label="Email address">
-        <Input
+      <Stack spacing={3} sx={{ width: "100%" }}>
+        <TextField
+          label="Email"
           type="email"
           id="email"
           autoComplete="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          disabled={false}
+          fullWidth
+          size="small"
         />
-      </FormRowVertical>
-
-      <FormRowVertical label="Password">
-        <Input
+        <TextField
+          label="Password"
           type="password"
           id="password"
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          disabled={false}
+          fullWidth
+          size="small"
         />
-      </FormRowVertical>
-
-      <FormRowVertical>
-        <Button size="large" disabled={isLoading}>
-          {isLoading ? <SpinnerMini /> : "Log In"}
-        </Button>
-      </FormRowVertical>
+        <Stack spacing={1}>
+          <Button type="submit" variant="contained" fullWidth>
+            {isLoading ? <SpinnerMini /> : "Log In"}
+          </Button>
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            <Button type="submit" variant="outlined" fullWidth>
+              Signup
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
     </Form>
   );
 };
